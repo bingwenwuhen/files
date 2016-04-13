c90b029e7629f58136debc9c5579e568d239971b
xiaxuan <xiaxuan@haizhi.com>
2016-04-13 14:38:20 +0800

    开发

--- app/views.py
+++ app/views.py
@@ -3,8 +3,15 @@ from app import app
 from flask import request
 from flask import render_template
 from app import db
-
+from app.models import FileMetaData
+from flask import jsonify
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route("/list", methods=['GET',])
+def list():
+    files = FileMetaData.objects.order_by('-createdAt')
+    return jsonify(status='success', files=[file.to_json() for file in files])
